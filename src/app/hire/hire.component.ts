86b8f603327dbea9fade66d143b9ca8ed4164195
Stephan Haditsch <stephan-haditsch@hotmail.com>
2021-01-31 11:50:46 +0100

    trying to get the form to work

--- src/app/hire/hire.component.ts
+++ src/app/hire/hire.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-hire',
@@ -13,17 +12,4 @@ export class HireComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
-  // public noWhiteSpaceValidator(control: FormControl) {
-  //   const isWhitespace = (control.value || '').trim().length === 0;
-  //   const isValid = !isWhitespace;
-  //   return isValid ? null : { 'whitespace': true };
-  // }
-
-  // contact = new FormGroup({
-    
-  //   email: new FormControl('', this.noWhiteSpaceValidator),
-  //   subject: new FormControl(''),
-  //   message: new FormControl('')
-  // });
-}
+} 
