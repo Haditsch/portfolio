d57bb4f891de9ab6358e7a3739f4fbb275d0e9dc
Stephan Haditsch <stephan-haditsch@hotmail.com>
2021-01-31 18:22:12 +0100

    form cleanup after submit

--- src/app/hire/hire.component.ts
+++ src/app/hire/hire.component.ts
@@ -36,9 +36,9 @@ export class HireComponent implements OnInit {
           //backend error. If status is 200, then the message successfully sent
           if (err.status === 200) {
             alert("Your message has been sent!");
-            this.contactForm.value.email = "";
-            this.contactForm.value.subject = "";
-            this.contactForm.value.message = "";
+            this.contactForm.value.email.value = "";
+            this.contactForm.value.subject.value = "";
+            this.contactForm.value.message.value = "";
           } else {
             alert("Something went wrong when sending your message.");
             console.log('Error status:');
